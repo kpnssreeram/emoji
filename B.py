245e2b7da386fd9c4bff23d4289973d7b7a0916f
kpnssbv <kpns.sreeram@bazaarvoice.com>
2024-03-22 20:01:59 +0530

    Add functionality

--- B.py
+++ B.py
@@ -1,39 +1,20 @@
-def getDeployLabelByUniverse(universe):
+def getVpcByUniverse(universe):
     switch = {
-        'ci': 'ci-agent',
-        'cert': 'cert-agent',
-        'anon': 'anon-agent',
-        'uat': 'uat-agent',
-        'bazaar': 'bazaar-agent'
+        'ci': 'qa',
+        'cert': 'qa',
+        'anon': 'qa',
+        'uat': 'prod',
+        'bazaar': 'prod'
     }
-    return switch.get(universe, 'default-agent')
-
-def getServiceNameByUniverse(universe):
-    switch = {
-        'ci': 'ci-service',
-        'cert': 'cert-service',
-        'anon': 'anon-service',
-        'uat': 'uat-service',
-        'bazaar': 'bazaar-service'
-    }
-    return switch.get(universe, 'default-service')
+    return switch.get(universe, 'default')
 
 # This code will only run if the script is executed directly (not imported as a module)
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) < 3:
-        print("Usage: B.py <function_name> <universe>")
+    if len(sys.argv) < 2:
+        print("Usage: B.py <universe>")
         sys.exit(1)
 
-    function_name = sys.argv[1]
-    universe = sys.argv[2]
-
-    if function_name == 'getDeployLabelByUniverse':
-        result = getDeployLabelByUniverse(universe)
-    elif function_name == 'getServiceNameByUniverse':
-        result = getServiceNameByUniverse(universe)
-    else:
-        print(f"Invalid function name: {function_name}")
-        sys.exit(1)
-
-    print(result)
+    universe = sys.argv[1]
+    vpc = getVpcByUniverse(universe)
+    print(vpc)
